9ade1bfc22aff97de103758a20efdaed6b600ce4
아기새 <82822684+liebespaar93@users.noreply.github.com>
2023-09-19 12:58:58 +0900

    push

---
+++ docs/js/header/header.js
@@ -0,0 +1,5 @@
+
+function sidebar_toggle() {
+    document.getElementById("sidebar").classList.toggle('sidebar-none');
+    document.getElementById("main").classList.toggle('main-blur');
+}
---
+++ docs/js/include.js
@@ -0,0 +1,58 @@
+function innerHTML_file(elem, file, render) {
+    var xhr = new XMLHttpRequest();
+    var markdown_len, include_len;
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    if (render)
+                        elem.innerHTML = render.render(this.responseText);
+                    else
+                    {
+                        include_len = document.getElementsByTagName("include").length
+                        markdown_len = document.getElementsByTagName("markdown").length
+                        elem.innerHTML = this.responseText;
+                        if (markdown_len < document.getElementsByTagName("markdown").length)
+                            includeMD();
+                        if (include_len < document.getElementsByTagName("include").length)
+                            includeHTML();
+                    }
+                }
+                if (this.status == 404) {
+                    elem.innerHTML = "Page not found.";
+                }
+            }
+        }
+    }
+    xhr.open("GET", file, true);
+    xhr.send();
+}
+
+function includeHTML() {
+    var target, file;
+    target = document.getElementsByTagName("include");
+    for (var i = 0; i < target.length; i++) {
+        file = target[i].getAttribute("include-html");
+        if (file && !target[i].classList.contains('include-load')) {
+            innerHTML_file(target[i], file, null);
+            target[i].classList.add('include-load')
+        }
+    }
+}
+
+function includeMD() {
+    var target, file;
+    target = document.getElementsByTagName("markdown");
+    for (var i = 0; i < target.length; i++) {
+        file = target[i].getAttribute("include-md");
+        if (file && !target[i].classList.contains('include-load')) {
+            const md = new Remarkable()
+            innerHTML_file(target[i], file, md);
+            target[i].classList.add('include-load')
+        }
+    }
+}
+
+function include() {
+    includeHTML()
+}
